15d0b538fcf222cb2cb6583666f6777efbb7a780
Pislyakov Oleg <olegpislyakov@mail.ru>
2024-10-16 15:45:20 +0300

    .env api url

--- src/vars.ts
+++ src/vars.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const instance = axios.create({
     withCredentials: false, //NOTE - Разобратья
     // baseURL: "http://127.0.0.1:8000/",
-    baseURL: "https://aa76-85-91-197-195.ngrok-free.app/",
+    baseURL: import.meta.env.VITE_API,
 });
