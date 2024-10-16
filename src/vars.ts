cfd0ecd54ddea2f3ca5d8afc4724f976c85ea049
Pislyakov Oleg <olegpislyakov@mail.ru>
2024-10-16 15:40:18 +0300

    axios withCredentials false for CORS policy

--- src/vars.ts
+++ src/vars.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    withCredentials: true,
+    withCredentials: false, //NOTE - Разобратья
     // baseURL: "http://127.0.0.1:8000/",
     baseURL: "https://aa76-85-91-197-195.ngrok-free.app/",
 });
