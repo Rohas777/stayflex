0d36c348d142b3ff85e783206c2d2d8c8a7a9e3c
Pislyakov Oleg <olegpislyakov@mail.ru>
2024-10-20 18:32:40 +0300

    servers responsive fix 2

--- src/pages/Servers/index.tsx
+++ src/pages/Servers/index.tsx
@@ -65,8 +65,8 @@ function Main() {
             </div>
             <div className="mt-5 intro-y box p-5">
                 {/* BEGIN: Basic Select */}
-                <div className="grid grid-cols-12 gap-5">
-                    <div className="col-span-7">
+                <div className="grid grid-cols-12 gap-3 sm:gap-5">
+                    <div className="col-span-12 sm:col-span-7">
                         <TomSelect
                             value={select}
                             onChange={(e) => {
@@ -85,7 +85,7 @@ function Main() {
                     </div>
                     <Button
                         variant="primary"
-                        className="col-span-5 flex items-center justify-center shadow-md"
+                        className="col-span-12 sm:col-span-5 flex items-center justify-center shadow-md"
                     >
                         <Lucide
                             icon="Save"
